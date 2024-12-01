c72709e33d4c02985cdc091123626c54f0acb87f
Mike Haiduk <x29a100@gmail.com>
2024-12-01 20:41:33 +0100

    bugfixes

--- init.php
+++ init.php
@@ -5,26 +5,26 @@ onlyInDebug();
 
 query("DROP TABLE IF EXISTS `accounts`;");
 query("CREATE TABLE IF NOT EXISTS `accounts` (
-  `domain` varchar(16) COLLATE utf8_bin NOT NULL,
-  `address` varchar(256) COLLATE utf8_bin NOT NULL,
-  `prev_key` varchar(256) COLLATE utf8_bin NOT NULL,
-  `next_hash` varchar(256) COLLATE utf8_bin NOT NULL,
-  `delegate` varchar(256) COLLATE utf8_bin DEFAULT NULL,
-  `balance` double NOT NULL,
+    `domain` varchar(16) COLLATE utf8_bin NOT NULL,
+    `address` varchar(256) COLLATE utf8_bin NOT NULL,
+    `prev_key` varchar(256) COLLATE utf8_bin NOT NULL,
+    `next_hash` varchar(256) COLLATE utf8_bin NOT NULL,
+    `delegate` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+    `balance` double NOT NULL,
    CONSTRAINT id UNIQUE (`domain`,`address`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
 
 query("DROP TABLE IF EXISTS `trans`;");
 query("CREATE TABLE IF NOT EXISTS `trans` (
-  `domain` varchar(16) COLLATE utf8_bin NOT NULL,
-  `from` varchar(256) COLLATE utf8_bin NOT NULL,
-  `to` varchar(256) COLLATE utf8_bin NOT NULL,
-  `key` varchar(256) COLLATE utf8_bin NOT NULL,
-  `next_hash` varchar(256) COLLATE utf8_bin NOT NULL,
-  `delegate` varchar(256) COLLATE utf8_bin DEFAULT NULL,
-  `amount` double NOT NULL,    
-  `fee` double NOT NULL,        
-  `time` int(11) NOT NULL
+    `domain` varchar(16) COLLATE utf8_bin NOT NULL,
+    `from` varchar(256) COLLATE utf8_bin NOT NULL,
+    `to` varchar(256) COLLATE utf8_bin NOT NULL,
+    `key` varchar(256) COLLATE utf8_bin NOT NULL,
+    `next_hash` varchar(256) COLLATE utf8_bin NOT NULL,
+    `delegate` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+    `amount` double NOT NULL,    
+    `fee` double NOT NULL,        
+    `time` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
 //  PRIMARY KEY (`next_hash`)
 
