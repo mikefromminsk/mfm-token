d0e4b31eb634d544ddee817f53753843d56fcfc6
Mike Haiduk <x29a100@gmail.com>
2024-12-03 15:42:48 +0100

    update tokenomics

--- search.php
+++ search.php
@@ -8,7 +8,10 @@ $size = get_int(size, 10);
 $search_text = strtolower($search_text);
 
 $response[tokens] =
-    select("select * from tokens where `domain` like '%$search_text%'"
+    select("select * from tokens"
+        . " where `domain` = '$search_text'"
+        . " or `domain` like '%$search_text%'"
+        . " order by case when `domain` = '$search_text' then 0 else 1 end"
         . " limit " . $page * $size . ", $size");
 
 echo json_encode($response);
